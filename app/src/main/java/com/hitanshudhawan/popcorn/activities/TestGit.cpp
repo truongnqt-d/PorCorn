4cf740c448d6a38bfb9c4ee238794866846ec515
truongnqt-d <truongnqt.dev@gmail.com>
2022-08-09 22:46:42 +0700

    add test

---
+++ app/src/main/java/com/hitanshudhawan/popcorn/activities/TestGit.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Admin on 8/9/2022.
+//
+
+#include "TestGit.h"
+dsafd
---
+++ app/src/main/java/com/hitanshudhawan/popcorn/activities/TestGit.h
@@ -0,0 +1,16 @@
+//
+// Created by Admin on 8/9/2022.
+//
+
+#ifndef PORCORN_TESTGIT_H
+#define PORCORN_TESTGIT_H
+
+
+
+class TestGit {
+ pr
+};
+
+
+
+#endif //PORCORN_TESTGIT_H
